577f334e3b8995f7f78d2a2913f5bb45fed81c88
K.C. Hunter <kchunter@connectionseducation.com>
2015-02-28 21:28:05 -0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,21 @@
 var gulp            = require('gulp');
+var concat          = require('gulp-concat');
+var rename          = require('gulp-rename');
+var uglify          = require('gulp-uglify');
+var sass            = require('gulp-sass');
+var runSequence     = require('run-sequence');
+
+/* tasks */
+gulp.task('sassDev', function ()
+{
+    gulp.src('src/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('stylesDev', function ()
+{
+    return gulp.src[('src/css/styles.css')]
+        .pipe(concat('omr.css'))
+        .pipe(gulp.dest('src/css'));
+});
