2d42ce37f9f2f391dd2cfaa7b87b976dacc81a8d
K.C. Hunter <kchunter@connectionseducation.com>
2015-03-19 17:09:52 -0400

    updating

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp            = require('gulp');
+var clean 			= require('gulp-clean');
 var concat          = require('gulp-concat');
 var concatCSS       = require('gulp-concat-css');
 var rename          = require('gulp-rename');
+var replace 		= require('gulp-replace');
 var uglify          = require('gulp-uglify');
 var sass            = require('gulp-sass');
 var runSequence     = require('run-sequence');
@@ -9,6 +11,17 @@ var runSequence     = require('run-sequence');
 var buildDir        = 'bin/';
 
 /* tasks */
+
+/** javascript tasks **/
+gulp.task('buildJS', function ()
+{
+	gulp.src(['bower_components/modernizr/modernizr.js', 'bower_components/jquery/dist/jquery.min.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js', 'bower_components/angularjs/angular.min.js',
+		'src/js/main.js'])
+		.pipe(concat("omr.js"))
+		//.pipe(uglify("omr.js"))
+		.pipe(gulp.dest(buildDir));
+});
+/** end javascript tasks **/
 
 /** sass tasks **/
 gulp.task('sassDev', function ()
@@ -25,6 +38,13 @@ gulp.task('stylesDev', function ()
         .pipe(gulp.dest("src/css/"));
 });
 
+gulp.task('stylesBuild', function ()
+{
+    gulp.src(["bower_components/bootstrap/dist/css/bootstrap.css", "src/css/styles.css"])
+        .pipe(concat("omr.css"))
+        .pipe(gulp.dest("src/css/"));
+});
+
 gulp.task('buildSassDev', function (cb)
 {
     runSequence('sassDev', 'stylesDev', cb);
@@ -37,4 +57,37 @@ gulp.task('buildSassDev', function (cb)
 gulp.task('watch', function ()
 {
     gulp.watch('src/**/*.scss', ['buildSassDev']);
+});
+
+gulp.task('clean', function ()
+{
+	return gulp.src("bin", {read: false})
+		.pipe(clean());
+});
+
+gulp.task('renameBuild', function ()
+{
+	gulp.src('src/build.html')
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest(buildDir));
+});
+
+gulp.task('replacePath', function ()
+{
+	gulp.src(['src/css/omr.css'])
+		.pipe(replace('../fonts', 'fonts'))
+		.pipe(replace('../img', 'img'))
+		.pipe(gulp.dest('src/css'));
+});
+
+gulp.task('moveFilesBuild', function ()
+{
+	gulp.src(["src/css/*.*", "src/fonts/*.*", "src/img/**/*.*"], {base: "./src/"})
+		.pipe(gulp.dest(buildDir));
+});
+
+
+gulp.task('build', function (callback)
+{
+	runSequence('clean', 'buildJS', 'buildSassDev', 'stylesBuild', 'replacePath', 'renameBuild', 'moveFilesBuild', callback);
 });
